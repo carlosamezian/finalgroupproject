ee99c766eec9dbd73b560be56b3808ca7218c6f7
Carlos Amezian <carlosamezian@gmail.com>
2020-11-12 13:34:33 -0500

    this first trial

---
+++ deliverable.py
@@ -0,0 +1,26 @@
+
+
+class TaskReminder():
+    def __init__(self):
+        
+        self.thetime = thetime
+        self.duedate = duedate
+        self.reminderfreq = reminderfreq
+        self.task = task
+        
+    
+    def set_time(self):
+        #to set the time of the task
+        
+        pass
+        
+    def set_date(self):
+        #to ser the date of the task
+        
+        pass
+    
+    def set_reminder(self):
+        #to set the reminder frequency
+        
+        pass
+    
