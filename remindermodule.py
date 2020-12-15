f73e2d936b88c4ca8c9e33fa70d5dee4598aae6b
Carlos Amezian <carlosamezian@gmail.com>
2020-12-14 23:16:10 -0500

    Updates

--- remindermodule.py
+++ remindermodule.py
@@ -1,32 +1,45 @@
 import time
-import winsound
 from win10toast import ToastNotifier
 
 
+class Remind:
+
+    def __init__(self, text, time):
+        """The attributes that class will hold.
+
+        Args:
+            text (string): the reminder that the user states.
+            time (int): the time it takes for the reminder to pop up
+        """
+        self.text = text
+        self.mytime = time
+
+    def notif(self):
+        """This method will contril the notifications. It will wait that specifiied time before printing the
+        notification. It will display it and make a sound.
+        
+        Args:
+            None
+        """
+        mynotif = ToastNotifier()
+        mynotif.show_toast("Reminder:", "Will go off in {} min(s) ".format(
+            self.mytime), duration=10)
+        time.sleep(self.mytime * 60)
+        mynotif.show_toast("Reminder: {}".format(self.text), duration=10)
+
+
 def main():
-    print("What shall I remind you about?")
-    text = str(input())
+    """The main function to ask the user for reminder and time.
+    """
+    text = input("What would you like to be reiminded about? ")
+    time = float(input("In how long?(minutes)"))
 
-    print("In how many minutes?")
+    myremind = Remind(text, time)
 
-    local_time = float(input())
-
-    local_time = local_time * 60
-
-    time.sleep(local_time)
-
-    print(text)
-
-
-def reminders(remind, time):
-    notif = ToastNotifier()
-    notif.show_toast("Reminder:", f""" Goes of in {time} secs""", duration=20)
-    notif.show_toast(f"Reminder", remind, duration=20)
-
-    freq = 2500
-    dur = 1000
-    winsound.Beep(freq, dur)
+    myremind.notif()
 
 
 if __name__ == "__main__":
-    reminders("Test", 20)
+    """Main functuon.
+    """
+    main()
