d785037458830409d0ad04e06ff9f350d11c1125
Campyb <74320234+Campyb@users.noreply.github.com>
2020-12-20 22:12:21 -0500

    Update groupmanage.py

--- groupmanage.py
+++ groupmanage.py
@@ -2,12 +2,12 @@ import random
 import csv
 import os.path
 import re
+import tkinter as tk
+
 
 class GroupManage:
     """Class for adding and removing users from a CSV file
-
     Attributes: None
-
     """
     def __init__(self):
         """
@@ -18,7 +18,6 @@ class GroupManage:
         appcsv: Variable for appending to CSV file
         headers (list): CSV file header names
         writer: Uses Dictwriter function from CSV module
-
         """
         self.filename = "user.csv" # Name of the file
         self.lis = [] # List of user information
@@ -30,37 +29,17 @@ class GroupManage:
             if not self.exists:
                 self.writer.writeheader() # Writes header names in CSV file if they aren't already there
         
-    def add_user(self):
-        """Adds a user to CSV file
+    def add_user(self, name, email):
+        """Sets up user info for GUI
         
         Args:
         filename (string): The name of the file
-        readcsv: Reads the CSV file
         lis (list): List of user information
         app: Appends user information to CSV file
         appcsv: Variable for appending to CSV file
         
         Returns: None
         """
-        name = input("Enter full name: ") # User's name
-        for i in name: # Error handling for name
-            while i.isnumeric():
-                name = input("Type in a valid name: ")
-                for i in name:
-                    if i.isnumeric() == False:
-                        continue
-        
-        email = input("Enter email address: ") # User's email address
-        while re.search('[\w\d.-_]+\.\w+', email) == None: # Error handling for email address syntax
-            email = input("Enter a valid email address: ")
-        userlist = [] # List of user information in each row
-        with open (self.filename, 'r') as self.readcsv: # Converts CSV rows to a list to check if the inputted email address is in the list
-            reader = csv.reader(self.readcsv)
-            for row in reader:
-                userlist.append(row)
-                for field in row:
-                    while email in row:
-                        email = input("Email address already taken! Enter another email address: ")
         
         password = random.randint(1000000000,9999999999) # Random 10 digit password for user
             
@@ -69,22 +48,21 @@ class GroupManage:
         with open(self.filename, 'a', newline='') as self.appcsv: # Appending user information to CSV file
             self.app = csv.writer(self.appcsv)
             self.app.writerow(self.lis)
+        
+        root=tk.Tk()
+        label1=tk.Label(root, text="Success you may close the previous window or type in a new user")
+        label1.pack()
 
-    def drop_user(self):
-        """Drops a user from CSV file
-
+    def drop_user(self, user):
+        """Sets up user drop for GUI
         Args:
         filename (string): The name of the file
         readcsv: Variable for reading CSV file
         writecsv: Variable for writing to CSV file
 
-
         Returns: None
         """
         userlist = [] # List of user information in each row
-        user = input('Email of user to remove: ') # User to be removed
-        while re.search('[\w\d.-_]+\.\w+', user) == None: # Error handling for email address
-            user = input("Type in an email address: ")
         with open (self.filename, 'r') as self.readcsv: # Converts CSV rows to a list and removes inputted user information from list
             reader = csv.reader(self.readcsv)
             for row in reader:
@@ -95,3 +73,30 @@ class GroupManage:
         with open (self.filename, 'w') as self.writecsv: # Overwrites original CSV file to update it
             writer = csv.writer(self.writecsv)
             writer.writerows(userlist)
+        root=tk.Tk()
+        label1=tk.Label(root, text="Success you may close previous window or type another user")
+        label1.pack()
+        
+
+myGroup=GroupManage()
+
+root=tk.Tk()
+
+label1=tk.Label(root, text= "Enter User Info Below (Case Sensitive): ")
+label1.grid(row=0,column=0)
+
+entry1=tk.Entry(root, width=35) 
+entry1.grid(row=1, column=1)
+entry1.insert("end", 'Student Name Here')
+
+entry2=tk.Entry(root, width=35) 
+entry2.grid(row=2, column=1)
+entry2.insert("end", 'Student Email Here')
+
+Add_User_Button=tk.Button(root, text="Add User", command= lambda: myGroup.add_user(entry1.get(),entry2.get()))
+Add_User_Button.grid(row=3,column=0)
+
+Drop_User_Button=tk.Button(root, text="Drop User",command= lambda: myGroup.drop_user(entry2.get()))
+Drop_User_Button.grid(row=3,column=1)
+
+root.mainloop()
